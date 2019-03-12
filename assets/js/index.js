3192c76f4029412dde36760b19ca9be8dfb16842
iheathdesigns <ianheath2010@gmail.com>
2019-03-12 17:20:19 -0400

    Updated the styling
    Added responsive styling and updated naming

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,40 @@
+
+// Modal Button Functions
 document.querySelector(".button-modal").addEventListener("click", function() {
     document.querySelector(".modal").style.display = 'flex';
 });
 
 document.querySelector('.close-btn').addEventListener("click", function() {
     document.querySelector('.modal').style.display = 'none';
+});
+
+// Rate table
+jQuery(document).ready(function($) {
+    $.getJSON("code-test.json", function(data){
+        var testData = [];
+        $.each(data, function(key, value){
+            testData += '<tr>';
+            testData += '<td>' + value.name + '</td>';
+            testData += '<td>' + value.apy + ' %' + '</td>';
+            testData += '<td>' + '$' + value.earnings + '</td>';
+            testData += '</tr>';
+        });
+        $('#rate-table').append(testData);
+    });
+});
+
+
+// Tabbed sidebar
+jQuery(document).ready(function($) {
+    $(".panel").hide();
+    $("ul.tabs li:first").addClass("tabs-active").show();
+    $(".panel:first").show();
+    $("ul.tabs li").click(function() {
+        $("ul.tabs li").removeClass("tabs-active");
+        $(this).addClass("tabs-active");
+        $(".panel").hide();
+        var activeLink = $(this).find("a").attr("href");
+        $(activeLink).fadeIn();
+        return false;
+    });
 });
